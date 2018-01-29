962014e5357a6bb85c9805bc67e3f8d21e1342e5
luminous3 <githubuser342@gmail.com>
2018-01-29 15:54:55 -0800

    add id to UserType

--- client/index.js
+++ client/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+});
 
 const Root = () => {
-  return (
-    <div>
-      Auth Starter
-    </div>
-  );
+  return <div>Auth Starter</div>;
 };
 
 ReactDOM.render(<Root />, document.querySelector('#root'));
