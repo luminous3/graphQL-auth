e9b7b16a949d7bf05241cb386c8a98749bcc5b97
luminous3 <githubuser342@gmail.com>
2018-01-31 06:37:05 -0800

    retrieve errors from graphql

--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -5,11 +5,23 @@ import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
 
 class LoginForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errors: []
+    };
+  }
   onSubmit({ email, password }) {
-    this.props.mutate({
-      variables: { email, password },
-      refetchQueries: [{ query }]
-    });
+    this.props
+      .mutate({
+        variables: { email, password },
+        refetchQueries: [{ query }]
+      })
+      .catch(res => {
+        const errors = res.graphQLErrors.map(error => error.message);
+        this.setState(errors);
+      });
   }
 
   render() {
