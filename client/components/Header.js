74b141ef4c2d464437dadedbaf705d4582ecc7a5
luminous3 <githubuser342@gmail.com>
2018-01-31 05:10:43 -0800

    not signed in state, add buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import query from '../queries/CurrentUser';
+import { Link } from 'react-router';
 
 class Header extends Component {
   renderButtons() {
@@ -11,14 +12,28 @@ class Header extends Component {
     if (user) {
       return <div>Logout</div>;
     } else {
-      return <div>You're not signed in</div>;
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
     }
   }
 
   render() {
     return (
       <nav>
-        <div className="nav-wrapper">{this.renderButtons()}</div>
+        <div className="nav-wrapper">
+          <Link to="/" className="brand-logo left">
+            Home
+          </Link>
+          <ul className="right">{this.renderButtons()}</ul>
+        </div>
       </nav>
     );
   }
